a8fa8fc3e80756b00f073f21a3265a84ad56449a
rajesukh <cparajesh2668@gmail.com>
2022-06-12 21:10:46 +0530

    opopop

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://rajesukh2668:E5sB52YEdmRqpQTu@cluster0.rzotr.mongodb.net/rajesukh2688-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
